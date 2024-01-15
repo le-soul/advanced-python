9d523906ba03fb99720705195efecf7ce6317fe5
le-soul <155693711+le-soul@users.noreply.github.com>
2024-01-15 16:19:43 +0100

    Write test and code for simple functions

---
+++ tests/test_simple_functions.py
@@ -0,0 +1,27 @@
+"""
+Script to write the test for the sum and subtract functions
+"""
+import unittest
+from scripts.simple_functions import sum_two_nums, sub_two_nums
+
+class TestSimpleFunction(unittest.TestCase):
+    """
+    Test simple functions sum & subtract
+    """
+
+    def test_sum(self):
+        """
+        Test the sum function
+        """
+        result = sum_two_nums(4,5)
+        self.assertEqual(result,9)
+
+    def test_sub(self):
+        """
+        Test the sub function
+        """
+        result = sub_two_nums(1,3)
+        self.assertEqual(result,-2)
+
+if __name__ == "__main__":
+    unittest.main()
