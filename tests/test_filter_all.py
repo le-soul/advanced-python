5568f78e016155e42dc5bbaba4f126285d695c73
le-soul <155693711+le-soul@users.noreply.github.com>
2024-01-17 17:31:24 +0100

    Update errors 2

--- tests/test_filter_all.py
+++ tests/test_filter_all.py
@@ -17,7 +17,20 @@ class TestFilterAll(unittest.TestCase):
         }
         self.df = pd.DataFrame(data)
 
-    def test_months(self):
-        filter_all()
+    def test_filter_by_month(self):
+        result_df = filter_all(self.df, month='January')
+        assert result_df.shape[0] == 1 
+
+    def test_filter_by_year(self):
+        result_df = filter_all(self.df, year=2022)
+        assert result_df.shape[0] == 1 
+
+    def test_filter_by_price(self):
+        result_df = filter_all(self.df, price=15.0)
+        assert result_df.shape[0] == 2 
+
+    def test_no_filters(self):
+        result_df = filter_all(self.df)
+        assert result_df.shape[0] == 4
 
 
